9387d39fb9b0fa1a876966ee2f8c2922c34557b4
Deepshikha1303 <deepshikhamishraa13@gmail.com>
2020-04-20 13:46:07 +0000

    Changed external api

--- app.py
+++ app.py
@@ -1,74 +1,65 @@
-from flask import Flask, render_template, request, jsonify,session, redirect, g
-import uuid
+from flask import Flask, render_template, request, jsonify, session
+import requests
+import json
 from cassandra.cluster import Cluster
+import csv
+#import requests_cache
+#requests_cache.install_cache('crime_api_cache', backend='sqlite', expire_after=36000)
+
+jokes_url_template = 'http://api.icndb.com/jokes/random?exclude=Array'
 
 cluster = Cluster(contact_points=['172.17.0.2'], port=9042)
 session = cluster.connect()
 app = Flask(__name__)
 
-@app.route('/pokemon', methods=['GET'])
-def pokemons():
+@app.route('/jokes', methods=['POST','GET'])
+def getjoke():
+    response = requests.get(jokes_url_template)
+    if response.ok:
+        parsed = response.json()
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>",parsed)
+        joke = parsed['value']['joke'].replace("'","")
+
+        insert = session.execute("""INSERT INTO jokes.data (Id, Joke) VALUES ({}, '{}')""".format(parsed['value']['id'], joke))
+        return jsonify(parsed)
+    else:
+        return jsonify(response.reason)
+
+@app.route('/getjokes', methods=['GET'])
+def alljokes():
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-    rows = session.execute("""SELECT * from pokemon.stats""")
+    rows = session.execute("""SELECT * from jokes.data""")
     print(rows._current_rows)
     k=rows._current_rows
     if rows:
-        return jsonify({'data':k})
-    return jsonify({'error':'No data present'})
+        return jsonify({'data':k}), 200
+    return jsonify({'error':'No data present'}), 404
 
-@app.route('/pokemon/<name>', methods=['GET'])
-def one_pokemon(name):
-    #name = request.json.get('name')
-    name=name.capitalize()
-    print('name>>>>>>>',name)
-    rows = session.execute("""SELECT * FROM pokemon.stats where name = '{}' ALLOW FILTERING""".format(name))
+@app.route('/getjokes/<id>', methods=['GET'])
+def one_joke(id):
+    rows = session.execute("""SELECT * FROM jokes.data where id = {} ALLOW FILTERING""".format(id))
     print('>>>>>>>>>>>>>>>>>>>>>>>',rows._current_rows)
     if rows:
-        return jsonify({'data':rows[0]})
-    return jsonify({'message':'no such pokemon exists'})
+        return jsonify({'data':rows[0]}), 200
+    return jsonify({'message':'Joke for this ID does not exist'}), 404
 
-@app.route('/pokemon/<name>/<type1>/<type2>/<total>/<hp>/<attack>/<defence>/<spAttack>/<spDefence>/<speed>/<generation>/<legendary>', methods=['POST', 'GET'])
-def add_pokemon(name, type1, type2, total, hp, attack, defence, spAttack, spDefence, speed, generation, legendary):
+@app.route('/getjokes/update/<id>/<joke>', methods=['PUT','GET'])
+def update_joke(id, joke):
 
-    name=name.capitalize()
-    type1=type1.capitalize()
-    type2=type2.capitalize()
-    legendary=legendary.capitalize()    
-    print("test>>>>>>>>>>>>>>>>>>>>",name,"    ",type1,"     ",type2,"    ",total,"      ",legendary)
-    #if not request.json or not name:
-    if not name:
-        return jsonify({'error':'The pokemon needs to have a name'}), 400
-    rows = session.execute("""SELECT * FROM pokemon.stats WHERE name = '{}' ALLOW FILTERING""".format(name))
-    if rows:
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",rows[0][0])
-        if rows[0].name==name:
-            return jsonify({'message':'Pokemon already exists', 'pokemon':rows[0]}), 400
-    insert = session.execute("""INSERT INTO pokemon.stats (Name, Type1, Type2, Total, HP, Attack, Defence, SpAttack, SpDefence, Speed, Generation, Legendary) VALUES ('{}','{}','{}',{},{},{},{},{},{},{},{},{})""".format(name, type1, type2, total, hp, attack, defence, spAttack, spDefence, speed, generation, legendary))
-    return jsonify({'Name':name, 'Type1':type1, 'Type2':type2, 'Total':total, 'HP':hp, 'Attack':attack, 'Defence':defence, 'SpAttack':spAttack, 'SpDefence':spDefence, 'Speed':speed, 'Generation':generation, 'Legendary':legendary})
-
-@app.route('/pokemon/update/<name>/<total>/<attack>/<defence>/<speed>', methods=['PUT','GET'])
-def update_characteristics(name, total, attack, defence, speed):
-
-    name=name.capitalize()
-    #only total, attack, defence, and speed are updateable
-    rows = session.execute("""SELECT * FROM pokemon.stats WHERE name='{}' ALLOW FILTERING""".format(name))
+    rows = session.execute("""SELECT * FROM jokes.data WHERE id={} ALLOW FILTERING""".format(id))
     if not rows:
-        return jsonify({'error':'No such pokemon exists'})
-    rows_update = session.execute("""UPDATE pokemon.stats set Total={}, Attack={}, Defence={}, Speed={} WHERE Name='{}'""".format(total, attack, defence, speed,name))
-    return jsonify({'Name':name, 'NewTotal':total, 'NewAttack':attack, 'Newdefence':defence, 'NewSpeed':speed})
+        return jsonify({'error':'No such jokeID exists'}), 404
+    rows_update = session.execute("""UPDATE jokes.data set Joke='{}' WHERE id={}""".format(joke, id))
+    return jsonify({'ID':id,'NewJoke':joke}), 200
 
-@app.route('/pokemon/delete/<name>', methods=['DELETE','GET'])
-def delete_pok(name):
-    name=name.capitalize()
-    #row = session.execute("""DELETE * FROM table pokemon.stats WHERE Name='{}'""".format(name))
-    prepared_statement=session.prepare('SELECT * FROM pokemon.stats WHERE name=?')
-    rows=session.execute(prepared_statement, (name,))
-    if rows:
-        prepared_statement=session.prepare('DELETE from pokemon.stats WHERE name=?')
-        rows=session.execute(prepared_statement, (name,))
-        print('>>>>>>>>>>>>>>>>>>>>>>>>',rows._current_rows)
-        return jsonify({'Name':name, 'Message':'Deleted'})
-    return jsonify({"message":"This pokemon doesn't exist"})
+@app.route('/getjokes/delete/<id>', methods=['DELETE','GET'])
+def delete_joke(id):
+    row=session.execute("""SELECT * FROM jokes.data WHERE id={}""".format(id))
+    if row:
+        deletedRows=session.execute("""DELETE from jokes.data WHERE id={}""".format(id))
+        print('>>>>>>>>>>>>>>>>>>>>>>>>',deletedRows._current_rows)
+        return jsonify({'ID':id, 'Message':'Deleted'}), 200
+    return jsonify({"message":"This joke doesn't exist"}), 404
 
 if __name__=='__main__':
 	app.run(host='0.0.0.0', port=80)
